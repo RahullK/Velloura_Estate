65ec96a00e4ab07ab4a3785eb1cd00df6a38a7ab
RahullK <50859983+RahullK@users.noreply.github.com>
2026-06-28 15:07:20 +0530

    Update settings.py

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -130,6 +130,14 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,

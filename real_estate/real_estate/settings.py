1adf41b04e13dd7a77c9a7ec34ff401f3cbae32d
RahullK <50859983+RahullK@users.noreply.github.com>
2026-07-14 14:40:37 +0530

    Modify database settings for SQLite path
    Updated database settings to use BASE_DIR for path.

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -76,11 +76,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'real_estate.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "/var/data/db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
